42e2a062b46e1d05c8cfe8bdb917845d81070787
phanhieukhang1999 <phanhieukhang1999gmail.com>
2022-10-28 23:26:08 +0700

    upload code trang admin

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ import Home from './pages/Home/Home';
 import News from './pages/News/News';
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register';
-import  CheckoutTemplate  from './templates/CheckoutTemplate/CheckoutTemplate';
+import CheckoutTemplate from './templates/CheckoutTemplate/CheckoutTemplate';
 import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
 
-import { Suspense, lazy} from 'react';
+import { Suspense, lazy } from 'react';
 import { UserTemplate } from './templates/UserTemplate/UserTemplate';
 import Loading from './components/Loading/Loading';
 import Profile from './pages/Profile/Profile';
+import AdminTemplate from './templates/AdminTemplate/AdminTemplate';
+import Dashboard from './pages/Admin/Dashboard/Dashboard';
+import Films from './pages/Admin/Films/Films';
+import Showtime from './pages/Admin/Showtime/Showtime';
+import AddNew from './pages/Admin/Films/AddNew/AddNew';
+import Edit from './pages/Admin/Films/Edit/Edit';
 
 // const CheckoutTemplateLazy = lazy(() => import('./templates/CheckoutTemplate/CheckoutTemplate'))
 
@@ -23,7 +29,7 @@ export const history = createBrowserHistory()
 function App() {
   return (
     <Router history={history}>
-      <Loading/>
+      <Loading />
       <Switch>
         <HomeTemplate path="/home" exact Component={Home} />
         <HomeTemplate path="/contact" exact Component={Contact} />
@@ -31,11 +37,17 @@ function App() {
         <HomeTemplate path="/detail/:id" exact Component={Detail} />
         <HomeTemplate path="/profile" exact Component={Profile} />
 
+        <CheckoutTemplate path="/checkout/:id" exact Component={Checkout} />
+
         <UserTemplate path="/register" exact Component={Register} />
-        
         <UserTemplate path="/login" exact Component={Login} />
 
-        <CheckoutTemplate path="/checkout/:id" exact Component={Checkout}/>
+        <AdminTemplate path="/admin" exact Component={Dashboard} />
+        <AdminTemplate path="/admin/users" exact Component={Dashboard} />
+        <AdminTemplate path="/admin/films" exact Component={Films} />
+        <AdminTemplate path="/admin/films/addnew" exact Component={AddNew} />
+        <AdminTemplate path="/admin/films/edit/:id" exact Component={Edit} />
+        <AdminTemplate path="/admin/films/showtime/:id/:tenphim" exact Component={Showtime} />
         {/* <Suspense fallback={<h1>LOADING ...</h1>}>
         <CheckoutTemplateLazy path="/checkout/:id" exact Component={Checkout} />
         </Suspense> */}
